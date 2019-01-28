6b16d266d9aa42d2a19bf092fac5ece07e044471
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 19:49:42 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,8 +3,10 @@ import React, {Component} from 'react';
 export default class GifList extends Component {
 
   render(){
-    <ul>
-      {this.props.gifs.map((gif,id) => <li key={id}>{gif}</li>)}
-    </ul>
+    return(
+      <ul>
+        {this.props.gifs.map((gif,id) => <li key={id}>{gif}</li>)}
+      </ul>
+    )
   }
 }
