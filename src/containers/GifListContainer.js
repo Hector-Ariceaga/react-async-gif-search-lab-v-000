142b9ca1f5ac553f2390cb8cd4763e5bd6490149
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 20:16:13 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -5,7 +5,6 @@ import GifSearch from '../compoonents/GifSearch.js'
 export default class GifListContainer extends Component{
   state = {
     gifs = []
-    query = undefined
   }
 
   render(){
@@ -17,21 +16,16 @@ export default class GifListContainer extends Component{
     )
   }
 
-  onSearchSubmit = (input) => {
-    this.setState({
-      query: input
-    })
+  onSearchSubmit = () => {
+    
   }
 
   componentDidMount() {
-    query = this.state.query
     fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`)
     .then(res => res.json())
     .then(data => {
-      gifs = data.slice(0,2)
-      urls = gifs.images.original.url
       this.setState({
-        gifs: urls
+        gifs: data.map(gif => ({url: gif.images.original.url}))
       })
     })
   }
