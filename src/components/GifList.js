25e50d9729d693ff6351dd16d7f081765b9b7404
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 20:24:35 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class GifList extends Component {
-
-  GifList = props => {
+export default const GifList = props => {
     return(
       <ul>
-        {props.gifs.map((gif) =>
+        {props.gifs.map((gif) => 
           <li key={gif.url}>
             <img src={gif.url} alt='gif'/>
           </li>
