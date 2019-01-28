950d3662d19d824f257428b198d9bbf2d0cdb6e3
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 20:23:13 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -5,7 +5,11 @@ export default class GifList extends Component {
   GifList = props => {
     return(
       <ul>
-        {props.gifs.map((gif) => <li key={gif.url}><img src={gif.url} alt='gif'/></li>)}
+        {props.gifs.map((gif) => 
+          <li key={gif.url}>
+            <img src={gif.url} alt='gif'/>
+          </li>
+        )}
       </ul>
     )
   }
