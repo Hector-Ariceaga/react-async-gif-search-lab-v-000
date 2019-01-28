8789e16bdfc0a34df78ed033faecb8477c2114dd
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 19:57:27 +0000

    Automatically backed up by Learn

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -5,4 +5,18 @@ export default class GifSearch extends Component {
   state = {
     input = undefined
   }
+
+  handleChange = () => {
+    this.setState({
+      input: value
+    })
+      
+    }
+  }
+  
+  render(){
+    return(
+      <input type="text" value={this.state.input} onChange={this.handleChange} >
+    )
+  }
 }
